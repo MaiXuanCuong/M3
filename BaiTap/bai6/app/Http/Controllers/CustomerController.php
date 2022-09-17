7f7b55f2f70b29fd337f3c5f0d9b4659168a4925
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-09-17 20:08:17 +0700

    hahaha

--- BaiTap/bai6/app/Http/Controllers/CustomerController.php
+++ BaiTap/bai6/app/Http/Controllers/CustomerController.php
@@ -11,12 +11,21 @@ class CustomerController extends Controller
 {
     public function index()
     {
-        $customers = Customer::all();
+        $customers = Customer::paginate(5);
         $cities = City::all();
         return view('customers.list', compact('customers', 'cities'));
     }
 
-
+    public function search(Request $request)
+    {
+        $keyword = $request->input('keyword');
+        if (!$keyword) {
+            return redirect()->route('customers.index');
+        }
+        $customers = Customer::where('name', 'LIKE', '%' . $keyword . '%')->paginate(5);
+        $cities = City::all();
+        return view('customers.list', compact('customers', 'cities'));
+    }
     public function create()
 {
     $cities = City::all();
