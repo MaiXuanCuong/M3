1d9c9f3bf528a8670e30aea8556d342c39e8471b
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-09-19 21:04:40 +0700

    hehehhe

---
+++ BaiTap/bai7/app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('users.index');
+    }
+
+    public function search(Request $request)
+{
+    {
+        try {
+            $user = User::findOrFail($request->input('user_id'));
+        } catch (ModelNotFoundException $exception) {
+            return back()->withErrors($exception->getMessage())->withInput();
+        }
+
+        return view('users.search', compact('user'));
+    }
+}
+    
+}
--- BaiTap/bai7/routes/web.php
+++ BaiTap/bai7/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('users.index');
 });
+Route::get('/users', [UserController::class,'index'])->name('users.index');
+Route::post('/users/search', [UserController::class,'search'])->name('users.search');
+
+ 
