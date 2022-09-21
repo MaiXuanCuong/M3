41e2020e8edb6703458337307292257d803a819a
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-09-21 21:21:53 +0700

    ahha

--- BaiTap/bai8/routes/web.php
+++ BaiTap/bai8/routes/web.php
@@ -27,3 +27,12 @@ Route::get('/blog', [BlogController::class, 'showBlog'])->name('show.blog');
 Route::get('/logout', [LogoutController::class, 'logout'])->name('user.logout');
 Route::get('products', [ProductController::class, 'index'])->name('index');
 Route::get('products/{id}', [ProductController::class, 'show'])->name('show');
+Route::get('/',[\App\Http\Controllers\ShopController::class,'index'])->name('shop.index');
+
+ 
+
+Route::get('/carts',[\App\Http\Controllers\ShopController::class,'getCart'])->name('shop.getCart');
+
+ 
+
+Route::get('/{idProduct}/add-to-cart', [\App\Http\Controllers\ShopController::class,'addToCart'])->name('add-to-cart');
